c7128b6197f222e8c5f94be30034c7dba8938bf5
Ihor Oherchuk <igor.ogerchuk@gmail.com>
2025-05-15 11:33:41 +0200

    fixed tailwind settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
-import "./globals.css"
+import "./styles/global.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -21,6 +21,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-
-import './globals.css'
